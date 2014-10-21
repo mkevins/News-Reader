2ceaa217d05566b1feed81685fa1d09dd2ded3cd
Matthew Kevins <mmkevins@yahoo.com>
2014-10-21 15:49:24 -0400

    finish nested entries

--- skeleton/app/assets/javascripts/collections/feeds.js
+++ skeleton/app/assets/javascripts/collections/feeds.js
@@ -1,4 +1,14 @@
 NewsReader.Collections.Feeds = Backbone.Collection.extend({
 	model: NewsReader.Models.Feed,
-	url: '/api/feeds'
+	url: '/api/feeds',
+
+	entries: function () {
+
+		this._entries = this._entries || new NewsReader.Collections.Entries([], {
+				feed: this
+			})
+
+		return this._entries
+
+	}
 });
--- skeleton/app/assets/javascripts/models/feed.js
+++ skeleton/app/assets/javascripts/models/feed.js
@@ -1,3 +1,17 @@
 NewsReader.Models.Feed = Backbone.Model.extend({
-	urlRoot: '/api/feeds'
+	urlRoot: '/api/feeds',
+	parse: function (jsonResp) {
+		if (jsonResp.latest_entries) {
+			this.entries().set(jsonResp.latest_entries);
+			delete jsonResp.latest_entries;
+		}
+		return jsonResp;
+	},
+
+	entries: function () {
+		this._entries = this._entries || new NewsReader.Collections.Entries([], {
+			feed: this
+		});
+		return this._entries;
+	}
 });
